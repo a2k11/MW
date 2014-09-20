3ae58f0f277eb2a9b9805cfa3c01698e72f2e3db
a2k11 <adam.kaewell@gmail.com>
2014-09-20 18:55:25 -0400

    Adding Program Directory

---
+++ Learn_to_Program/calc.rb
@@ -0,0 +1,11 @@
+puts "Hours in a year: "
+puts ( 24 * 365 )
+puts "Minutes in a decade: "
+puts ( 60 * 24 * 365 * 10 )
+puts "Seconds old I am :"
+puts ( 60 * 60 * 24 * 365 * 30 )
+puts "Chocolates I hope to eat in my life :"
+puts ( 365 * 100 )
+puts "If you are 1202 million seconds old, you are :"
+puts ( 1202000000 / ( 60 * 60 * 24 * 365 ) )
+
